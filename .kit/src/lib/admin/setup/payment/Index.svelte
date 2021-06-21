f75931a7dc8741742874033c472667f4b0e09363
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2021-06-21 14:17:55 +0100

    None and Tokens payment types now report state

--- .kit/src/lib/admin/setup/payment/Index.svelte
+++ .kit/src/lib/admin/setup/payment/Index.svelte
@@ -1,175 +1,13 @@
 <script>
-  import Switch from 'svelte-switch'
-  import { TabbedInterface, TabList, Tab, TabPanel } from '$lib/TabbedInterface'
-  import SensitiveTextInput from '$lib/SensitiveTextInput.svelte'
   import ServiceState from '../ServiceState.js'
-  // import StatusMessage from '$lib/StatusMessage.svelte'
-
-  import { PAYMENT_PROVIDERS } from '$lib/Constants'
 
   import PaymentProviderNone from './None.svelte'
   import PaymentProviderTokens from './Tokens.svelte'
   import PaymentProviderStripe from './Stripe.svelte'
 
-  import {
-    alphabeticallySortedCurrencyDetails,
-    additionalCurrenciesSupportedInUnitedArabEmirates
-  } from '$lib/StripeCurrencies.js'
-
   export let settings
   export let socket
   export let state = new ServiceState()
-
-  const gotPrice = {
-    test: false,
-    live: false
-  }
-
-  const priceError = {
-    test: null,
-    live: null
-  }
-
-  let stripePrice = 10
-  let previousStripePrice = stripePrice
-  let formattedMinimumPrice
-  let stripeCurrency
-  let stripeCurrencyOnlyValidInUnitedArabEmirates = false
-  let minimumStripePriceForCurrency = 1
-
-  $: stripeCurrencyOnlyValidInUnitedArabEmirates = additionalCurrenciesSupportedInUnitedArabEmirates.includes(stripeCurrency)
-
-  // Custom validation because the built-in browser form validation is useless.
-  // This simply does not allow an invalid value to be entered.
-  function validateStripePriceOnInput (event) {
-    const price = event.target.value
-    if (price < 1 || parseInt(price) === NaN) {
-      // On input, do not force the validation so people
-      // can create temporarily invalid enties (e.g., when deleting an existing
-      // number)
-      return
-    } else {
-      previousStripePrice = stripePrice
-    }
-  }
-
-  // On change, actually force the value to be correct.
-  function validateStripePriceOnChange (event) {
-    const price = event.target.value
-    if (price < 1 || parseInt(price) === NaN) {
-      stripePrice = previousStripePrice
-    }
-  }
-
-  const type = {
-    SETTINGS: 'settings',
-    VALIDATE_SETTINGS: 'price'
-  }
-
-  const messageIsOf = (type) => type
-  const errorIsOf = (type) => `${type}-error`
-
-  // TODO: Note: the logic here has changed since we’re going to create the price
-  // ===== via the Stripe API. Rewrite this.
-  function validateSettings(modeId) {
-    state.set(state.UNKNOWN)
-
-    switch (settings.payment.provider) {
-      case PAYMENT_PROVIDERS.none:
-        // The None payment provider doesn’t have any
-        // settings so it’s always valid.
-        state.set(state.OK)
-      break
-
-      case PAYMENT_PROVIDERS.token:
-        // TODO: Token payment type not implemented yet.
-        state.set(state.NOT_OK)
-      break
-
-      case PAYMENT_PROVIDERS.stripe:
-        gotPrice[modeId] = false
-        priceError[modeId] = null
-
-        const priceId = (settings.payment.providers[2].modeDetails[modeId === 'test' ? 0 : 1].priceId).trim()
-
-        if (priceId === '') {
-          return
-        }
-
-        if (
-          !(
-            (priceId.length === 1 && priceId === 'p') ||
-            (priceId.length === 2 && priceId === 'pr') ||
-            (priceId.length === 3 && priceId === 'pri') ||
-            (priceId.length === 4 && priceId === 'pric') ||
-            (priceId.length === 5 && priceId === 'price') ||
-            (priceId.length >= 6 && priceId.startsWith('price_'))
-          )
-        ) {
-          priceError[modeId] = 'That is not a valid price ID. It must start with price_'
-          gotPrice[modeId] = true
-          state.set(state.NOT_OK)
-          return
-        }
-
-        if (priceId.length !== 30) {
-          priceError[modeId] = null
-          gotPrice[modeId] = false
-          state.set(state.NOT_OK)
-          return
-        }
-
-        console.log('Getting price…')
-        socket.send(JSON.stringify({
-          type: 'get-price',
-          mode: modeId
-        }))
-        console.log(gotPrice, priceError)
-      break
-    }
-  }
-
-  socket.addEventListener('message', event => {
-    const message = JSON.parse(event.data)
-
-    switch (message.type) {
-      case messageIsOf(type.SETTINGS):
-        validateSettings()
-      break
-
-      case messageIsOf(type.VALIDATE_SETTINGS):
-        settings.payment.providers[2].modeDetails[message.mode === 'test' ? 0 : 1].currency = currencies[message.currency]
-        settings.payment.providers[2].modeDetails[message.mode === 'test' ? 0 : 1].amount = message.amount
-        gotPrice[message.mode] = true
-        priceError[message.mode] = null
-        state.set(state.OK)
-
-        state.set(state.OK)
-      break
-
-      case errorIsOf(type.VALIDATE_SETTINGS):
-        if (message.error.param === 'price' && message.error.type === 'invalid_request_error') {
-          priceError[message.mode] = 'No price exists with that API ID.'
-        } else {
-          priceError[message.mode] = `${message.error.message} (${message.error.type})`
-        }
-        gotPrice[message.mode] = true
-
-        state.set(state.NOT_OK, { error: message.error })
-      break
-    }
-  })
-
-  // TODO: Implement this in index and then remove from here.
-  // $: {
-  //   if (stripeCurrency) {
-  //     const currencyDetails = currencyDetailsForCurrencyCode(stripeCurrency)
-  //     let output = currencyDetails.template
-  //     output = output.replace('$', currencyDetails.symbol)
-  //     output = output.replace('1', minimumStripePriceForCurrency)
-  //     formattedMinimumPrice = output
-  //   }
-  // }
 </script>
 
 {#if settings}
@@ -182,7 +20,6 @@
     name='paymentProvider'
     id='paymentProvider'
     bind:value={settings.payment.provider}
-    on:change={validateSettings}
     size={settings.payment.providers.length}
   >
     {#each settings.payment.providers as provider, index}
@@ -191,130 +28,20 @@
   </select>
 
   {#if settings.payment.provider === 0}
-    <!-- None: no payment provider. All server setups must be done via the admin. -->
-    <PaymentProviderNone />
+    <PaymentProviderNone bind:state={state} />
   {/if}
 
   {#if settings.payment.provider === 1}
-    <!-- Tokens -->
-    <PaymentProviderTokens />
+    <PaymentProviderTokens {settings} {socket} bind:state={state}/>
   {/if}
 
   {#if settings.payment.provider === 2}
-    <!-- Stripe. -->
-    <section class='instructions'>
-      <h4>Instructions</h4>
-
-      <ol>
-        <li>Get a <a href='https://stripe.com'>Stripe</a> account.</li>
-        <li>Accept your <a href='https://stripe.com/dpa/legal'>Data Processing Addendum</a> (GDPR). Download and print a copy, sign it and keep it safe.</li>
-        <li>From your <a href='https://dashboard.stripe.com/dashboard'>Stripe dashboard</a>, get your <em>test API keys</em> and your live API keys and enter them below.</li>
-        <li>Set the price and currency to finish your Stripe configuration and create your monthly subscription. Please also read through the <a href='https://stripe.com/docs/currencies'>supported currencies</a> section of the Stripe documentation.</li>
-      </ol>
-
-      <p><em></em></p>
-    </section>
-
-    <h5>Subscription</h5>
-
-    <p>Please note that you can only have one plan, only set prices in whole numbers (no “psychological pricing”), and only support one currency (ideally, the one for the local region that your Small Web Domain is based in). These limitations are not bugs, they are features to encourage a Small Web. <a href='#payment-notes'>Learn more.</a></p>
-
-    <label for='currency'>Currency</label>
-
-    <select id='currency' bind:value={stripeCurrency}>
-      {#each alphabeticallySortedCurrencyDetails as currency, index}
-        <option value={currency.code} selected={currency.code === 'eur'}>{currency.label}</option>
-      {/each}
-    </select>
-
-    {#if stripeCurrencyOnlyValidInUnitedArabEmirates}
-      <p><small><strong>* This currency is only supported if your organisation is set to United Arab Emirates in Stripe.</strong> For more information, please see the <a href='https://stripe.com/docs/currencies'>supported currencies</a> section of the Stripe documentation.</small></p>
-    {/if}
-
-    <label for='price'>Price/month</label>
-    <input id='price' type='number' bind:value={stripePrice} step=1 min=1 on:input={validateStripePriceOnInput} on:change={validateStripePriceOnChange}/>
-
-    <label for='mode'>Mode</label>
-    <Switch id='mode' on:change={event => settings.payment.providers[2].mode = event.detail.checked ? 'live' : 'test'} checked={settings.payment.providers[2].mode === 'live'} handleDiameter='' width=75>
-
-      <span class='live' slot='checkedIcon'>Live</span>
-      <span class='test' slot='unCheckedIcon'>Test</span>
-    </Switch>
-
-    <TabbedInterface navStyle={true}>
-      <TabList navStyle={true}>
-        {#each settings.payment.providers[2].modeDetails as mode}
-          <Tab navStyle={true}>{mode.title}</Tab>
-        {/each}
-      </TabList>
-      {#each settings.payment.providers[2].modeDetails as mode}
-        <TabPanel>
-          <h4>{mode.title}</h4>
-          <p>The necesssary objects will be created for you automatically on Stripe once you add your Stripe keys.</p>
-
-          <ol class='serverCreationProgress'>
-            <!-- <li><StatusMessage state={ok.stripeProduct}>Product</StatusMessage></li>
-            <li><StatusMessage state={ok.stripePrice}>Price</StatusMessage></li>
-            <li><StatusMessage state={ok.stripeWebhook}>Webhook</StatusMessage></li> -->
-          </ol>
-          <label for={`${mode.id}PublishableKey`}>Publishable key</label>
-          <input id={`${mode.id}PublishableKey`} type='text' bind:value={mode.publishableKey} on:input={validateSettings(mode.id)}/>
-
-          <label class='block' for={`${mode.id}SecretKey`}>Secret Key</label>
-          <!-- TODO: Implement input event on SensitiveTextInput component. -->
-          <SensitiveTextInput name={`${mode.id}SecretKey`} bind:value={mode.secretKey} on:input={validateSettings(mode.id)}/>
-
-          {#if gotPrice[mode.id] && priceError[mode.id] !== null}
-            <p style='color: red;'>❌️ {priceError[mode.id]}</p>
-          {:else if gotPrice[mode.id]}
-            <p>✔️ Based on your Stripe {mode.id} mode product settings, your hosting price is set for <strong>{mode.currency}{mode.amount}/month.</p>
-          {:else}
-            <p>ℹ️ <em>Please enter your API key details to create your monthly subscription.</em></p>
-          {/if}
-
-          <section id='payment-notes'>
-            <h5>A note on commerical payment support.</h5>
-
-            <p>When taking commercial payments for your Small Web Domain via Stripe, you can only have one plan, only set prices in whole numbers (no “psychological pricing”), and only support one currency (ideally, the one for the local region that your Small Web Domain is based in). These limitations are not bugs, they are features to encourage a Small Web.</p>
-
-            <p>The idea is that no single Small Web Domain should scale beyond a certain point. Your Small Web Domain should be serving your community and you should let other Small Web Domains serve theirs. This is our <a href='https://small-web.org/about/#small-technology'>non-colonial approach</a> as per the <a href='https://small-web.org/about/#small-technology'>Small Technology Principles</a>.</p>
-
-            <p>Support for a commercial option is necessary for organisations that have to exist under capitalism. It doesn’t mean we have to play their shortsighted manipulative games or adopt their success criteria. The goal is for our organisations to provide a bridge to a post-capitalist future (e.g., on where cities can use tokens to provide their citizens with access to the commons from the commons).</p>
-
-            <p>You will not become rich by running a Small Web Domain. If that’s your goal, please look elsewhere. However, you will hopefully be able to susbist under capitalism while helping bootstrap a kinder, fairer, and more caring world based on respect for human rights and democracy.</p>
-
-            <p><strong>If you are making money with your Small Web Domain, please consider sharing a percentage of your earnings with <a href='https://small-tech.org/'>Small Technology Foundation</a> by <a href='https://small-tech.org/fund-us'>becoming a patron</a> so we can continue to develop the software you use to run yours.</strong></p>
-          </section>
-        </TabPanel>
-      {/each}
-    </TabbedInterface>
+    <PaymentProviderStripe {settings} {socket} bind:state={state} />
   {/if}
 {/if}
 
 <style>
-  #paymentProvider {
+  select {
     min-width: 300px;
   }
-
-  :global(label[for=mode] + div) {
-    margin-bottom: 1em;
-  }
-
-  label[for=mode] {
-    display: block;
-    margin-bottom: 0;
-  }
-
-  .live, .test {
-		color: white;
-		display: inline-block;
-		margin-top: 0.1em;
-	}
-  .test {
-    margin-right: 0.75em;
-  }
-
-	.live {
-		margin-left: 0.75em;
-	}
 </style>
