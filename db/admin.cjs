d52c4891d71639851548f578df3d68cfbcf241a2
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2021-10-29 16:04:43 +0100

    Committing uncommitted WIP changes

--- db/admin.cjs
+++
@@ -1,2 +0,0 @@
-globalThis._ = { 'password': `b2d5300cd71f5be93a810f75361d3244` };
-(function () { if (typeof define === 'function' && define.amd) { define([], globalThis._); } else if (typeof module === 'object' && module.exports) { module.exports = globalThis._ } else { globalThis.admin = globalThis._ } })();
